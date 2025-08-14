a1ad7c8d80d470c691fff84470e4a75e1f2df505
BB9z <bb9z@me.com>
2025-08-14 22:07:46 +0800

    Format test cases

--- tests/folding-algorithm.test.ts
+++ tests/folding-algorithm.test.ts
@@ -38,24 +38,18 @@ function check(
 
 describe('Folding Algorithm Tests', () => {
 	it('should not fold when file is small', () => {
-		const symbols: UTSymbol[] = [
-			[
-				'TestClass',
-				SymbolKind.Class,
-				1,
-				20,
-				[
+		// biome-ignore format: skip
+		check('test.ts', 30, [
+			['TestClass', SymbolKind.Class, 1, 20, [
 					['m1', SymbolKind.Method, 5, 10, []],
 					['m2', SymbolKind.Method, 12, 18, []],
-				],
-			],
-		];
-		check('test.ts', 30, symbols, [], []);
+			]],
+		], [], []);
 	});
 
 	it('should fold nested symbols up to depth 2', () => {
-		const symbols: UTSymbol[] = [
-			// biome-ignore format: skip
+		// biome-ignore format: skip
+		check('nested.ts', 80, [
 			['L1', SymbolKind.Function, 1, 80, [
 				['L2', SymbolKind.Function, 2, 79, [
 					['L3', SymbolKind.Function, 3, 78, [
@@ -63,136 +57,94 @@ describe('Folding Algorithm Tests', () => {
 					]],
 				]],
 			]],
-		];
-		check('nested.ts', 80, symbols, [], [[3, 78]]);
+		], [], [
+			[3, 78]
+		]);
 	});
 
 	it('should fold methods in a class when file is large', () => {
-		const symbols: UTSymbol[] = [
-			[
-				'Manager',
-				SymbolKind.Class,
-				13,
-				116,
-				[
-					['m1', SymbolKind.Method, 33, 41, []],
-					['p1', SymbolKind.Property, 15, 15, []],
-					['m4', SymbolKind.Method, 77, 85, []],
-					['m2', SymbolKind.Method, 18, 23, []],
-					['m3', SymbolKind.Method, 48, 62, []],
-				],
-			],
-		];
-		// All methods should be folded
-		check(
-			'manager.ts',
-			120,
-			symbols,
-			[],
-			[
-				[18, 23],
-				[33, 41],
-				[48, 62],
-				[77, 85],
-			],
-		);
+		// biome-ignore format: skip
+		check('manager.ts', 120, [
+			['Manager', SymbolKind.Class, 13, 116, [
+				['m1', SymbolKind.Method, 33, 41, []],
+				['p1', SymbolKind.Property, 15, 15, []],
+				['m4', SymbolKind.Method, 77, 85, []],
+				['m2', SymbolKind.Method, 18, 23, []],
+				['m3', SymbolKind.Method, 48, 62, []],
+			]],
+		], [], [
+			// All methods should be folded
+			[18, 23],
+			[33, 41],
+			[48, 62],
+			[77, 85],
+		]);
 	});
 
 	it('should fold interfaces and enums when space is limited', () => {
-		const symbols: UTSymbol[] = [
-			[
-				'interface 1',
-				SymbolKind.Interface,
-				9,
-				54,
-				[
-					['p1', SymbolKind.Property, 11, 11, []],
-					['m1', SymbolKind.Method, 53, 53, []],
-					['m2', SymbolKind.Method, 43, 43, []],
-				],
-			],
-			['interface 2', SymbolKind.Interface, 56, 61, [['hide', SymbolKind.Method, 60, 60, []]]],
-			[
-				'enum1',
-				SymbolKind.Enum,
-				64,
-				69,
-				[
-					['enum v1', SymbolKind.Variable, 66, 66, []],
-					['enum v2', SymbolKind.Variable, 68, 68, []],
-				],
-			],
-		];
-		check(
-			'interfaces.ts',
-			180,
-			symbols,
-			[],
-			[
-				[9, 54],
-				[56, 61],
-				[64, 69],
-			],
-		);
+		// biome-ignore format: skip
+		check('interfaces.ts', 180, [
+			['interface 1', SymbolKind.Interface, 9, 54, [
+				['p1', SymbolKind.Property, 11, 11, []],
+				['m1', SymbolKind.Method, 53, 53, []],
+				['m2', SymbolKind.Method, 43, 43, []],
+			]],
+			['interface 2', SymbolKind.Interface, 56, 61, [
+				['hide', SymbolKind.Method, 60, 60, []]
+			]],
+			['enum1', SymbolKind.Enum, 64, 69, [
+				['enum v1', SymbolKind.Variable, 66, 66, []],
+				['enum v2', SymbolKind.Variable, 68, 68, []],
+			]],
+		], [], [
+			[9, 54],
+			[56, 61],
+			[64, 69],
+		]);
 	});
 
 	it('should handle test files specially - fold containers but not describe blocks', () => {
-		const symbols: UTSymbol[] = [
-			[
-				'some tests',
-				SymbolKind.Function,
-				17,
-				125,
-				[
-					['describe 1', SymbolKind.Function, 18, 64, []],
-					['describe 2', SymbolKind.Function, 66, 124, []],
-				],
-			],
-		];
-		// In test files, describe blocks shouldn't be folded, but outer containers should be
-		check('some.test.ts', 130, symbols, [], [[17, 125]]);
+		// biome-ignore format: skip
+		check('some.test.ts', 130, [
+			['some tests', SymbolKind.Function, 17, 125, [
+				['describe 1', SymbolKind.Function, 18, 64, []],
+				['describe 2', SymbolKind.Function, 66, 124, []],
+			]],
+		], [], [
+			[17, 125]
+		]);
 	});
 
 	it('should fold large variables but not small ones', () => {
-		const symbols: UTSymbol[] = [
+		// biome-ignore format: skip
+		check('data.ts', 110, [
 			['array', SymbolKind.Variable, 1, 24, []],
-			[
-				'class',
-				SymbolKind.Class,
-				26,
-				100,
-				[
-					['m1', SymbolKind.Method, 31, 49, []],
-					['m2', SymbolKind.Method, 74, 99, []],
-				],
-			],
+			['class', SymbolKind.Class, 26, 100, [
+				['m1', SymbolKind.Method, 31, 49, []],
+				['m2', SymbolKind.Method, 74, 99, []],
+			]],
 			['smallVar', SymbolKind.Variable, 102, 105, []],
-		];
-		check(
-			'data.ts',
-			110,
-			symbols,
-			[],
-			[
-				[1, 24],
-				[31, 49],
-				[74, 99],
-			],
-		);
+		], [], [
+			[1, 24],
+			[31, 49],
+			[74, 99],
+		]);
 	});
 
 	it('should respect size thresholds based on remaining space', () => {
-		const symbols: UTSymbol[] = [
-			['SmallMethod', SymbolKind.Method, 1, 4, []], // 4 lines
-			['MediumMethod', SymbolKind.Method, 6, 15, []], // 10 lines
-			['LargeMethod', SymbolKind.Method, 17, 50, []], // 34 lines
-		];
-		// Should prioritize folding large methods, may fold medium methods based on remaining space, generally don't fold small methods
-		check('methods.ts', 60, symbols, [], [[17, 50]]);
+		// biome-ignore format: skip
+		check('methods.ts', 60, [
+			['SmallMethod', SymbolKind.Method, 1, 4, []],
+			['MediumMethod', SymbolKind.Method, 6, 15, []],
+			['LargeMethod', SymbolKind.Method, 17, 50, []],
+		], [], [
+			[17, 50]
+		]);
 	});
 
 	it('should fold all top-level symbols in large files', () => {
-		const symbols: UTSymbol[] = [
+		// biome-ignore format: skip
+		check('schema file', 418, [
 			['f1', SymbolKind.Function, 2, 5, []],
 			['db', SymbolKind.Struct, 7, 10, []],
 			['c1', SymbolKind.Class, 15, 51, []],
@@ -207,247 +159,152 @@ describe('Folding Algorithm Tests', () => {
 			['c8', SymbolKind.Class, 194, 208, []],
 			['e3', SymbolKind.Enum, 210, 217, []],
 			['c9', SymbolKind.Class, 220, 238, []],
-		];
-		check(
-			'schema file',
-			418,
-			symbols,
-			[],
-			[
-				[2, 5],
-				[7, 10],
-				[15, 51],
-				[55, 67],
-				[69, 75],
-				[78, 92],
-				[97, 125],
-				[127, 135],
-				[137, 147],
-				[149, 152],
-				[155, 191],
-				[194, 208],
-				[210, 217],
-				[220, 238],
-			],
-		);
+		], [], [
+			[2, 5],
+			[7, 10],
+			[15, 51],
+			[55, 67],
+			[69, 75],
+			[78, 92],
+			[97, 125],
+			[127, 135],
+			[137, 147],
+			[149, 152],
+			[155, 191],
+			[194, 208],
+			[210, 217],
+			[220, 238],
+		]);
 	});
 
 	it('should correctly fold nested symbols with multi-level children', () => {
-		const symbols: UTSymbol[] = [
+		// biome-ignore format: skip
+		check('big-func.ts', 59, [
 			['root var', SymbolKind.Variable, 17, 17, []],
-			[
-				'root func',
-				SymbolKind.Function,
-				19,
-				57,
-				[
-					[
-						'v1',
-						SymbolKind.Property,
-						21,
-						24,
-						[
-							['v1-1', SymbolKind.Property, 22, 22, []],
-							['v1-2', SymbolKind.Property, 23, 23, []],
-						],
-					],
-					['v5', SymbolKind.Variable, 49, 49, []],
-					['v4', SymbolKind.Variable, 36, 44, []],
-					['v3', SymbolKind.Variable, 33, 33, []],
-					['v2', SymbolKind.Variable, 31, 31, []],
-				],
-			],
-		];
-		check(
-			'big-func.ts',
-			59,
-			symbols,
-			[],
-			[
-				[19, 57],
+			// biome-ignore format: skip
+			['root func', SymbolKind.Function, 19, 57, [
+				['v1', SymbolKind.Property, 21, 24, [
+					['v1-1', SymbolKind.Property, 22, 22, []],
+					['v1-2', SymbolKind.Property, 23, 23, []],
+				]],
+				['v5', SymbolKind.Variable, 49, 49, []],
+				['v4', SymbolKind.Variable, 36, 44, []],
+				['v3', SymbolKind.Variable, 33, 33, []],
+				['v2', SymbolKind.Variable, 31, 31, []],
+			]],
+		], [], [
+			[19, 57],
 				[21, 24],
 				[36, 44],
-			],
-		);
+		]);
 	});
 
 	it('should fold deeply nested properties', () => {
-		const symbols: UTSymbol[] = [
-			[
-				'p1',
-				SymbolKind.Variable,
-				10,
-				58,
-				[
-					[
-						'p2',
-						SymbolKind.Property,
-						11,
-						57,
-						[
-							[
-								'p3-1',
-								SymbolKind.Property,
-								45,
-								50,
-								[
-									['center', SymbolKind.Property, 47, 47, []],
-									['end', SymbolKind.Property, 48, 48, []],
-									['justify', SymbolKind.Property, 49, 49, []],
-									['start', SymbolKind.Property, 46, 46, []],
-								],
-							],
-							[
-								'p3-2',
-								SymbolKind.Property,
-								37,
-								44,
-								[
-									['bold', SymbolKind.Property, 42, 42, []],
-									['extrabold', SymbolKind.Property, 43, 43, []],
-									['inherit', SymbolKind.Property, 38, 38, []],
-									['medium', SymbolKind.Property, 40, 40, []],
-									['regular', SymbolKind.Property, 39, 39, []],
-									['semibold', SymbolKind.Property, 41, 41, []],
-								],
-							],
-							[
-								'p3-3',
-								SymbolKind.Property,
-								26,
-								36,
-								[
-									['base', SymbolKind.Property, 27, 27, []],
-									['caution', SymbolKind.Property, 34, 34, []],
-									['critical', SymbolKind.Property, 35, 35, []],
-									['info', SymbolKind.Property, 32, 32, []],
-									['link', SymbolKind.Property, 29, 29, []],
-									['muted', SymbolKind.Property, 28, 28, []],
-									['primary', SymbolKind.Property, 30, 30, []],
-									['secondary', SymbolKind.Property, 31, 31, []],
-									['success', SymbolKind.Property, 33, 33, []],
-								],
-							],
-							['p3-4', SymbolKind.Property, 51, 53, [['true', SymbolKind.Property, 52, 52, []]]],
-							['p3-5', SymbolKind.Property, 54, 56, [['true', SymbolKind.Property, 55, 55, []]]],
-							[
-								'p3-6',
-								SymbolKind.Property,
-								12,
-								25,
-								[
-									['body-2xs', SymbolKind.Property, 20, 20, []],
-									['body-3xs', SymbolKind.Property, 19, 19, []],
-									['body-lg', SymbolKind.Property, 24, 24, []],
-									['body-md', SymbolKind.Property, 23, 23, []],
-									['body-sm', SymbolKind.Property, 22, 22, []],
-									['body-xs', SymbolKind.Property, 21, 21, []],
-									['heading-2xl', SymbolKind.Property, 18, 18, []],
-									['heading-lg', SymbolKind.Property, 16, 16, []],
-									['heading-md', SymbolKind.Property, 15, 15, []],
-									['heading-sm', SymbolKind.Property, 14, 14, []],
-									['heading-xl', SymbolKind.Property, 17, 17, []],
-									['heading-xs', SymbolKind.Property, 13, 13, []],
-								],
-							],
-						],
-					],
-				],
-			],
-			[
-				'p2',
-				SymbolKind.Variable,
-				60,
-				73,
-				[
-					['body-2xs', SymbolKind.Property, 68, 68, []],
-					['body-3xs', SymbolKind.Property, 67, 67, []],
-					['body-lg', SymbolKind.Property, 72, 72, []],
-					['body-md', SymbolKind.Property, 71, 71, []],
-					['body-sm', SymbolKind.Property, 70, 70, []],
-					['body-xs', SymbolKind.Property, 69, 69, []],
-					['heading-2xl', SymbolKind.Property, 66, 66, []],
-					['heading-lg', SymbolKind.Property, 64, 64, []],
-					['heading-md', SymbolKind.Property, 63, 63, []],
-					['heading-sm', SymbolKind.Property, 62, 62, []],
-					['heading-xl', SymbolKind.Property, 65, 65, []],
-					['heading-xs', SymbolKind.Property, 61, 61, []],
-				],
-			],
-			[
-				'TextProps',
-				SymbolKind.Interface,
-				75,
-				80,
-				[
-					['as', SymbolKind.Property, 77, 77, []],
-					['asChild', SymbolKind.Property, 78, 78, []],
-					['className', SymbolKind.Property, 79, 79, []],
-				],
-			],
-			[
-				'Text',
-				SymbolKind.Variable,
-				82,
-				120,
-				[
-					[
-						'forwardRef() callback',
-						SymbolKind.Function,
-						83,
-						119,
-						[
-							['align', SymbolKind.Property, 109, 109, []],
-							['className', SymbolKind.Property, 112, 112, []],
-							['fontWeight', SymbolKind.Property, 108, 108, []],
-							['Tag', SymbolKind.Variable, 98, 100, []],
-							['tone', SymbolKind.Property, 107, 107, []],
-							['truncate', SymbolKind.Property, 110, 110, []],
-							['uppercase', SymbolKind.Property, 111, 111, []],
-							['variant', SymbolKind.Property, 106, 106, []],
-						],
-					],
-				],
-			],
-		];
-		check(
-			'Text.tsx',
-			123,
-			symbols,
-			[],
-			[
-				[11, 57],
+		// biome-ignore format: skip
+		check('Text.tsx', 123, [
+			['p1', SymbolKind.Variable, 10, 58, [
+				['p2', SymbolKind.Property, 11, 57, [
+					['p3-1', SymbolKind.Property, 45, 50, [
+						['center', SymbolKind.Property, 47, 47, []],
+						['end', SymbolKind.Property, 48, 48, []],
+						['justify', SymbolKind.Property, 49, 49, []],
+						['start', SymbolKind.Property, 46, 46, []],
+					]],
+					['p3-2', SymbolKind.Property, 37, 44, [
+						['bold', SymbolKind.Property, 42, 42, []],
+						['extrabold', SymbolKind.Property, 43, 43, []],
+						['inherit', SymbolKind.Property, 38, 38, []],
+						['medium', SymbolKind.Property, 40, 40, []],
+						['regular', SymbolKind.Property, 39, 39, []],
+						['semibold', SymbolKind.Property, 41, 41, []],
+					]],
+					['p3-3', SymbolKind.Property, 26, 36, [
+						['base', SymbolKind.Property, 27, 27, []],
+						['caution', SymbolKind.Property, 34, 34, []],
+						['critical', SymbolKind.Property, 35, 35, []],
+						['info', SymbolKind.Property, 32, 32, []],
+						['link', SymbolKind.Property, 29, 29, []],
+						['muted', SymbolKind.Property, 28, 28, []],
+						['primary', SymbolKind.Property, 30, 30, []],
+						['secondary', SymbolKind.Property, 31, 31, []],
+						['success', SymbolKind.Property, 33, 33, []],
+					]],
+					['p3-4', SymbolKind.Property, 51, 53, [['true', SymbolKind.Property, 52, 52, []]]],
+					['p3-5', SymbolKind.Property, 54, 56, [['true', SymbolKind.Property, 55, 55, []]]],
+					['p3-6', SymbolKind.Property, 12, 25, [
+						['body-2xs', SymbolKind.Property, 20, 20, []],
+						['body-3xs', SymbolKind.Property, 19, 19, []],
+						['body-lg', SymbolKind.Property, 24, 24, []],
+						['body-md', SymbolKind.Property, 23, 23, []],
+						['body-sm', SymbolKind.Property, 22, 22, []],
+						['body-xs', SymbolKind.Property, 21, 21, []],
+						['heading-2xl', SymbolKind.Property, 18, 18, []],
+						['heading-lg', SymbolKind.Property, 16, 16, []],
+						['heading-md', SymbolKind.Property, 15, 15, []],
+						['heading-sm', SymbolKind.Property, 14, 14, []],
+						['heading-xl', SymbolKind.Property, 17, 17, []],
+						['heading-xs', SymbolKind.Property, 13, 13, []],
+					]],
+				]],
+			]], // End: p1
+			['p2', SymbolKind.Variable, 60, 73, [
+				['body-2xs', SymbolKind.Property, 68, 68, []],
+				['body-3xs', SymbolKind.Property, 67, 67, []],
+				['body-lg', SymbolKind.Property, 72, 72, []],
+				['body-md', SymbolKind.Property, 71, 71, []],
+				['body-sm', SymbolKind.Property, 70, 70, []],
+				['body-xs', SymbolKind.Property, 69, 69, []],
+				['heading-2xl', SymbolKind.Property, 66, 66, []],
+				['heading-lg', SymbolKind.Property, 64, 64, []],
+				['heading-md', SymbolKind.Property, 63, 63, []],
+				['heading-sm', SymbolKind.Property, 62, 62, []],
+				['heading-xl', SymbolKind.Property, 65, 65, []],
+				['heading-xs', SymbolKind.Property, 61, 61, []],
+			]],
+			['interface', SymbolKind.Interface, 75, 80, [
+				['as', SymbolKind.Property, 77, 77, []],
+				['asChild', SymbolKind.Property, 78, 78, []],
+				['className', SymbolKind.Property, 79, 79, []],
+			]],
+			['Text', SymbolKind.Variable, 82, 120, [
+				['func', SymbolKind.Function, 83, 119, [
+					['p1', SymbolKind.Property, 109, 109, []],
+					['p2', SymbolKind.Property, 112, 112, []],
+					['p3', SymbolKind.Property, 108, 108, []],
+					['v1', SymbolKind.Variable, 98, 100, []],
+					['p4', SymbolKind.Property, 107, 107, []],
+					['p5', SymbolKind.Property, 110, 110, []],
+					['p6', SymbolKind.Property, 111, 111, []],
+					['p7', SymbolKind.Property, 106, 106, []],
+				]],
+			]],
+		], [], [
+			[11, 57], // p2
 				[12, 25],
 				[26, 36],
 				[37, 44],
 				[45, 50],
 				[51, 53],
 				[54, 56],
-				[83, 119],
-			],
-		);
+			[83, 119], // func
+		]);
 	});
 
 	it('should handle empty symbols gracefully', () => {
-		const symbols: UTSymbol[] = [];
-		check('empty.ts', 10, symbols, [], []);
+		check('empty.ts', 10, [], [], []);
 	});
 
 	it('should respect skip ranges and not fold overlapping symbols', () => {
-		const symbols: UTSymbol[] = [
+		// biome-ignore format: skip
+		check('test-skip.ts', 50, [
 			['method1', SymbolKind.Method, 12, 15, []],
 			['method2', SymbolKind.Method, 25, 30, []],
 			['method3', SymbolKind.Method, 35, 45, []],
-		];
-		check(
-			'test-skip.ts',
-			50,
-			symbols,
-			[[40, 41]],
-			[
-				[12, 15],
-				[25, 30],
-			],
-		);
+		], [ 
+			[40, 41]
+		], [
+			[12, 15],
+			[25, 30],
+		]);
 	});
 });
